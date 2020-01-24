ccf58bed5ed514c653658cca2eb9f86c403019c6
Lee <a_lee100712@ats-hr420-dm28.fc.ca>
2020-01-24 12:16:51 -0500

    updating

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//This is a self-invoking
+//also called sn IIFE
+//also called a module
+
+(()	=> {
+	console.log("some message - from JavaScript");
+
+	let //theButton = document.querySelector("#myButton"),
+		theHeading = document.querySelector('h1'),
+		allSVGs = document.querySelectorAll(".svg");
+
+	// this function changes the heading text
+	/* function changeText() {
+	theHeading.textContent = "Hello there"; 
+}*/
+
+function logoSVG() {
+	console.log(this.id);
+}
+
+//theButton.addEventListener("click", changeText);
+
+
+allSVGs.forEach(item => item.addEventListener('click', logoSVG));
+
+
+})();
